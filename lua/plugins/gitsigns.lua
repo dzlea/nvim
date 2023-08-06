a455cbb05a90defd40519753374c513a2bb99679
l <dzxlzy@gmail.com>
2023-08-06 22:17:07 +0800

    add gitsigns with git commit message

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -1,14 +1,17 @@
 return {
     {
         "lewis6991/gitsigns.nvim",
-        opts = {
-            signs = {
-                add = { text = '+' },
-                change = { text = '~' },
-                delete = { text = '_' },
-                topdelete = { text = '‾' },
-                changedelete = { text = '~' },
-            },
-        },
+        config = function()
+            require('gitsigns').setup {
+                signs = {
+                    add = { text = '+' },
+                    change = { text = '~' },
+                    delete = { text = '_' },
+                    topdelete = { text = '‾' },
+                    changedelete = { text = '~' },
+                },
+                current_line_blame = true,
+            }
+        end
     },
 }
