14f4b8a7e8041edc8c87167d67724105e98f5253
l <dzxlzy@gmail.com>
2023-08-14 19:53:55 +0800

    add keymaps desc and new plugins

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -3,24 +3,24 @@ vim.g.mapleader = " "
 local keymap = vim.keymap
 
 --插入模式
-keymap.set("i", "jj", "<ESC>")
-keymap.set("i", "jk", "<ESC>")
-keymap.set("i", "kk", "<ESC>")
+keymap.set("i", "jj", "<ESC>", { desc = "Exit Insert Mode" })
+keymap.set("i", "jk", "<ESC>", { desc = "Exit Insert Mode" })
+keymap.set("i", "kk", "<ESC>", { desc = "Exit Insert Mode" })
 
 --视觉模式
 -- --单行或多行移动
-keymap.set("v", "J", ":m '>+1<CR>gv=gv")
-keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+keymap.set("v", "J", ":m '>+1<CR>gv=gv", { desc = "Move Down Single Or Multiple Lines" })
+keymap.set("v", "K", ":m '<-2<CR>gv=gv", { desc = "Move Up Single Or Multiple Lines" })
 
 --正常模式
 -- --水平增加窗口
-keymap.set("n", "<leader>sv", "<C-w>v")
+keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Horizontal Increase Window" })
 -- --垂直增加窗口
-keymap.set("n", "<leader>ss", "<C-w>s")
+keymap.set("n", "<leader>ss", "<C-w>s", { desc = "Vertically Increase Window" })
 
 --取消高亮
-keymap.set("n", "<leader>nh", ":nohl<CR>")
+keymap.set("n", "<leader>nh", ":nohl<CR>", { desc = "Unhighlight" })
 --保存退出
-keymap.set("n", "<leader>sq", ":wq<CR>")
+keymap.set("n", "<leader>sq", ":wq<CR>", { desc = "Save And Exit" })
 --保存
-keymap.set("n", "<leader>s", ":w<CR>")
+keymap.set("n", "<leader>s", ":w<CR>", { desc = "Save" })
